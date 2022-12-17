d9a2e7fb34dbc38e6adec10739bd3dd1aec2fc51
Kirill Karavaev <coffee.2016@yandex.ru>
2022-12-17 23:41:35 +0300

    Added 1st task

---
+++ DZ-1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите первое число: ");
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+
+if (number1 > number2) max=number1;
+if (number1 < number2) max=number2;
+
+Console.Write("max = ");
+Console.Write(max);
