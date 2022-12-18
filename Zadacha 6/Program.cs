18e8645a8d619bcfe2ea8c2992b39942d4ef747d
Kirill Karavaev <coffee.2016@yandex.ru>
2022-12-18 17:07:10 +0300

    Добавлено решение задачи 6 из дз

---
+++ Zadacha 6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число: ");
+
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if (Number % 2 == 0) 
+{
+    Console.WriteLine("Да, число четное");
+}
+else Console.WriteLine("Нет, число нечетное");
